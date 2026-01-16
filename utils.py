73bc6f3f481d523fc462e6824d9900dda2f46788
GirinoX45 <lubuntupython@gmail.com>
2026-01-16 18:10:03 -0300

    feat(utils): Adiciona a função auxiliar para a criação de matrizes
    * feat(utils): Função `cria_matriz` criada
    
    * docs(utils): Docstring para o módulo criada.
    
    * feat(utils): Bloco da função `main`, junto a condicional '__name__ == __main__' criado para depuração local.
    
    * feat(utils): Adicionado depuração simples para a função `cria_matriz` dentro da função `main`.
    
    * style(utils): Linting simples realizado para retirar duas linhas de código que não eram utilizadas pela função `cria_matriz`.

---
+++ utils.py
@@ -0,0 +1,44 @@
+"""
+Docstring para o módulo 'utils.py'.
+"""
+
+
+# Funções Auxiliares
+
+def cria_matriz(colunas:int,linhas:int) -> list[list[int]]:
+
+    ### Variáveis ###
+
+    #Valor que sera incrementado
+    valor = 0
+
+    #Lista da Matriz
+    lista_matriz = []
+
+    ### Código ###
+
+    #Vamos realizar a iteração para a criação da matriz
+    for linha in range(linhas):
+        lista_matriz.append([])
+
+        for _ in range(colunas):
+            valor += 1
+            lista_matriz[linha].append(valor)
+    
+    #Retornando a matriz
+    return lista_matriz
+
+
+# Função Main
+
+def main():
+    
+    matriz = cria_matriz(3,3)
+    for linha in matriz:
+        print(linha)
+
+
+if __name__ == "__main__":
+    main()
+
+
