d0895105d34517643ae9ae898039cbb20a31848a
Lucas Salles <lubuntupython@gmail.com>
2026-01-16 15:36:44 -0300

    feat: Adicioando função 'main' local no módulo 'base.py' para depuração.

--- base.py
+++ base.py
@@ -2,6 +2,9 @@
 Módulo teste para a revisão do GitHub
 """
 
+
+# Classes
+
 class Astride:
     def __init__(self):
         self.nome = "Astride Hofferson"
@@ -11,3 +14,14 @@ class Astride:
     
     def caracteristicas(self):
         return "Muito inteligente, e carinhosa tambem!"
+    
+
+# Função Main
+
+def main():
+    a = Astride()
+    print(a.caracteristicas())
+
+
+if __name__ == "__main__":
+    main()
