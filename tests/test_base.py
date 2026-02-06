9c3e03d182c994ecbea31af6da702a196f962e04
Lucas Salles <lubuntupython@gmail.com>
2026-02-06 18:27:02 -0300

    refactor(test_base): Colocado prefixo **test_** em frente as funções de `teste

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,8 +3,8 @@ Docstring for tests.test_base
 """
 
 
-def beleza_astride(qualidade:str) -> str:
+def test_beleza_astride(qualidade:str) -> str:
     return f"A astride é muito {qualidade}"
 
-def beleza_elfa(qualidade:str) -> str:
+def test_beleza_elfa(qualidade:str) -> str:
     return f"A Elfa é muito {qualidade}"
